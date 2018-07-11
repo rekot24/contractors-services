2f0c3fd1c44cd531dea5a9b165b2230357668022
Joshua Wallace <rekot24@gmail.com>
2018-07-11 15:47:27 -0700

    carousel updated

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -15,15 +15,23 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slides");
   var dots = document.getElementsByClassName("dots");
+
+  // put counter back to first slide if past count of slides
   if (n > slides.length) {slideIndex = 1}
+  // move counter to last slide if less than 1(first slide)
   if (n < 1) {slideIndex = slides.length}
+  // set display to "none" for all slides
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
+  // Remove active status for all "dots"
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+
+  // Set active slide/dot using current slide count
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000);
 }
+
+setInterval(plusSlides, 5000, slideIndex);
